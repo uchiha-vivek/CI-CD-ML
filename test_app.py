27298f332f72696ba62cfb07d5c91b6b7417c639
uchiha-vivek <vs160125@gmail.com>
2025-03-25 19:33:27 +0530

    Test case fixed

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response=app.test_client().get("/")
     assert response.status_code==200
-    assert response.data=="Hello World"
+    assert response.data==b"Hello World"
